dba1cdd22d32d9642f7f5c28df7931904f4f2e93
Michael Hope <mlhx@google.com>
2017-03-18 22:46:23 +0100

    pipoint: fixed the longitude calculation.  Change tests to check for NaN.

--- src/juju.net.nz/pipoint/latlon.go
+++ src/juju.net.nz/pipoint/latlon.go
@@ -5,20 +5,27 @@ import (
 )
 
 const (
-	a = 6378137.0 // in m
-	e = 298.257223563
+	m1 = 111132.92 // latitude calculation term 1
+	m2 = -559.82   // latitude calculation term 2
+	m3 = 1.175     // latitude calculation term 3
+	m4 = -0.0023   // latitude calculation term 4
+	p1 = 111412.84 // longitude calculation term 1
+	p2 = -93.5     // longitude calculation term 2
+	p3 = 0.118     // longitude calculation term 3
 )
 
 // LatLength returns the length of a line of latitude at the given
 // latitude.  Input is in rad, output in m.
 func LatLength(lat float64) float64 {
-	return 111132.954 - 559.822*math.Cos(2*lat) + 1.175*math.Cos(4*lat)
+	return m1 + m2*math.Cos(2*lat) + m3*math.Cos(4*lat)
 }
 
 // LonLength returns the length of a line of longitude at the given
 // latitude.  Input is in rad, output in m.
 func LonLength(lat float64) float64 {
-	sin := math.Sin(lat)
-	return math.Pi * a * math.Cos(lat) /
-		(180 * math.Sqrt(1-e*e*sin*sin))
+	return p1*math.Cos(lat) + p2*math.Cos(3*lat) + p3*math.Cos(5*lat)
+}
+
+func AsRad(deg float64) float64 {
+	return deg * math.Pi / 180
 }
--- src/juju.net.nz/pipoint/latlon_test.go
+++ src/juju.net.nz/pipoint/latlon_test.go
@@ -6,7 +6,10 @@ import (
 	"fmt"
 )
 
-func checkClose(t *testing.T, a float64, b float64, limit float64) {
+func checkClose(t *testing.T, a, b, limit float64) {
+	if math.IsNaN(a) || math.IsNaN(b) {
+		t.Errorf("Either %v or %v is NaN", a, b)
+	}
 	err := math.Abs(a - b)
 	if err >= limit {
 		t.Errorf("%v differs from %v by %v (more than %v)\n",
@@ -16,12 +19,12 @@ func checkClose(t *testing.T, a float64, b float64, limit float64) {
 
 func TestLatLength(t *testing.T) {
 	// Test vectors from https://en.wikipedia.org/wiki/Latitude
-	checkClose(t, LatLength(math.Pi*0/180), 110574, 1)
-	checkClose(t, LatLength(math.Pi*15/180), 110649, 1)
-	checkClose(t, LatLength(math.Pi*30/180), 110852, 1)
-	checkClose(t, LatLength(math.Pi*45/180), 111132, 1)
-	checkClose(t, LatLength(math.Pi*60/180), 111412, 1)
-	checkClose(t, LatLength(math.Pi*75/180), 111618, 1)
+	checkClose(t, LatLength(0*math.Pi/180), 110574, 1)
+	checkClose(t, LatLength(15*math.Pi/180), 110649, 1)
+	checkClose(t, LatLength(30*math.Pi/180), 110852, 1)
+	checkClose(t, LatLength(45*math.Pi/180), 111132, 1)
+	checkClose(t, LatLength(60*math.Pi/180), 111412, 1)
+	checkClose(t, LatLength(75*math.Pi/180), 111618, 1)
 }
 
 func ExampleLatLength() {
@@ -31,10 +34,10 @@ func ExampleLatLength() {
 
 func TestLonLength(t *testing.T) {
 	// Test vectors from https://en.wikipedia.org/wiki/Latitude
-	checkClose(t, LonLength(math.Pi*0/180), 111320, 1)
-	checkClose(t, LonLength(math.Pi*15/180), 107550, 1)
-	checkClose(t, LonLength(math.Pi*30/180), 96486, 1)
-	checkClose(t, LonLength(math.Pi*45/180), 78847, 1)
-	checkClose(t, LonLength(math.Pi*60/180), 55800, 1)
-	checkClose(t, LonLength(math.Pi*75/180), 28902, 1)
+	checkClose(t, LonLength(0*math.Pi/180), 111320, 1)
+	checkClose(t, LonLength(15*math.Pi/180), 107550, 1)
+	checkClose(t, LonLength(30*math.Pi/180), 96486, 1)
+	checkClose(t, LonLength(45*math.Pi/180), 78847, 1)
+	checkClose(t, LonLength(60*math.Pi/180), 55800, 1)
+	checkClose(t, LonLength(75*math.Pi/180), 28902, 1)
 }
