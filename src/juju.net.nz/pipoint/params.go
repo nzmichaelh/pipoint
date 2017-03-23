ae75d53a32a7e7fc9d903de994db84ec34722794
Michael Hope <mlhx@google.com>
2017-03-23 20:39:57 +0100

    params: split params out from param.

---
+++ src/juju.net.nz/pipoint/params.go
@@ -0,0 +1,105 @@
+package pipoint
+
+import (
+	"bytes"
+	"fmt"
+	"net/http"
+	"reflect"
+	"strings"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/common/expfmt"
+)
+
+type ParamListener func(p *Param)
+
+// Params is a group of parameters that can be listened to.
+type Params struct {
+	Name      string
+	params    []*Param
+	listeners []ParamListener
+}
+
+// Create a new Param in this group.  The Param is uninitialised and
+// invalid.
+func (ps *Params) New(name string) *Param {
+	p := &Param{
+		name:   name,
+		params: ps,
+	}
+	ps.params = append(ps.params, p)
+	return p
+}
+
+// Create a new, valid Param in this group using the given value.
+func (ps *Params) NewWith(name string, value interface{}) *Param {
+	p := &Param{
+		name:   name,
+		params: ps,
+	}
+	p.Set(value)
+	ps.params = append(ps.params, p)
+	return p
+}
+
+func (ps *Params) updated(p *Param) {
+	for _, l := range ps.listeners {
+		l(p)
+	}
+}
+
+// Listen to changes on any parameter in this group.
+func (ps *Params) Listen(l ParamListener) {
+	ps.listeners = append(ps.listeners, l)
+}
+
+func (ps *Params) Walk(visitor func(*Param)) {
+	for _, p := range ps.params {
+		visitor(p)
+	}
+}
+
+func makeName(path []string) string {
+	name := strings.Join(path, ".")
+	return strings.Replace(strings.ToLower(name), ".", "_", -1)
+}
+
+func (ps *Params) visitOne(w *bytes.Buffer, path []string, v reflect.Value) {
+	switch v.Kind() {
+	case reflect.Ptr, reflect.Interface:
+		ps.visitOne(w, path, v.Elem())
+	case reflect.Struct:
+		t := v.Type()
+		for i := 0; i < v.NumField(); i++ {
+			ps.visitOne(w, append(path, t.Field(i).Name), v.Field(i))
+		}
+	case reflect.String:
+		w.WriteString(fmt.Sprintf("%s{value=\"%s\"} 1\n", makeName(path), v))
+	default:
+		w.WriteString(fmt.Sprintf("%s %v\n", makeName(path), v))
+	}
+}
+
+// Respond with the Prometheus and Params as metrics.
+func (ps *Params) Metrics(w http.ResponseWriter, req *http.Request) {
+	var buf bytes.Buffer
+	enc := expfmt.NewEncoder(&buf, expfmt.FmtText)
+	mfs, err := prometheus.DefaultGatherer.Gather()
+
+	if err == nil {
+		for _, mf := range mfs {
+			enc.Encode(mf)
+		}
+	}
+
+	header := w.Header()
+	header.Set("Content-Type", string(expfmt.FmtText))
+
+	ps.Walk(func(p *Param) {
+		if p.Get() != nil {
+			ps.visitOne(&buf, []string{"pipoint", p.name}, reflect.ValueOf(p.Get()))
+		}
+	})
+
+	w.Write(buf.Bytes())
+}
