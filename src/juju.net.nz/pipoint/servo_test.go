f652e88e58a17641913b9b9194b3e71e84f8674a
Michael Hope <mlhx@google.com>
2017-03-25 20:32:09 +0100

    servo: update the tests.

--- src/juju.net.nz/pipoint/servo_test.go
+++ src/juju.net.nz/pipoint/servo_test.go
@@ -12,14 +12,14 @@ func TestServo(t *testing.T) {
 
 	assert.InDelta(t, s.sp.GetFloat64(), 0, 0.001, "Starts at zero")
 
-	s.Set(-math.Pi / 7)
-	assert.InDelta(t, s.pv.GetFloat64(), -math.Pi/7, 0.01)
-	s.Set(math.Pi / 3)
-	assert.InDelta(t, s.pv.GetFloat64(), math.Pi/3, 0.01)
+	s.Set(-math.Pi*0.5)
+	assert.InDelta(t, s.pv.GetFloat64(), 1.1, 0.01)
+	s.Set(+math.Pi*0.5)
+	assert.InDelta(t, s.pv.GetFloat64(), 1.9, 0.01)
 
 	// Stays within limits
-	s.Set(-math.Pi * 2 / 3)
-	assert.InDelta(t, s.pv.GetFloat64(), -math.Pi/2, 0.01)
-	s.Set(+math.Pi * 2 / 3)
-	assert.InDelta(t, s.pv.GetFloat64(), math.Pi/2, 0.01)
+	s.Set(-math.Pi*0.7)
+	assert.InDelta(t, s.pv.GetFloat64(), 1.0, 0.01)
+	s.Set(+math.Pi*0.7)
+	assert.InDelta(t, s.pv.GetFloat64(), 2.0, 0.01)
 }
